545ecf996359d3f928aba6a7718b00f3248ebb85
Jens Ramakers <jensramakers@gmail.com>
2023-05-11 13:47:10 +0200

    push.js testing 2

--- ElderEatsMobile/resources/js/app.js
+++ ElderEatsMobile/resources/js/app.js
@@ -12,10 +12,6 @@ let linkedAccountsArray = JSON.parse(linkedAccountsId);
 
 let test =  Math.floor(Math.random() * 10);
 
-Push.create(`Test message ${test}`, {
-    body: "Hey hoi hallo.",
-});
-
 linkedAccountsArray?.forEach((accountid) => {
     Echo.channel('product-scanned-channel-' + accountid)
         .listen('.add-product', (e) => {
@@ -29,3 +25,53 @@ linkedAccountsArray?.forEach((accountid) => {
             }
         });
 })
+
+
+
+
+
+// Request permission to send notifications
+function requestNotificationPermissions() {
+    Push.create("Permission Request", {
+        body: "Click here to enable notifications",
+        timeout: 5000,
+        onClick: function() {
+            this.close();
+            Push.Permission.request(onGranted, onDenied);
+        }
+    });
+}
+
+// Function to handle permission granted
+function onGranted() {
+    Push.create("Welcome", {
+        body: "You will now receive notifications",
+        timeout: 5000
+    });
+}
+
+// Function to handle permission denied
+function onDenied() {
+    Push.create("Permission Denied", {
+        body: "You have denied permission to receive notifications",
+        timeout: 5000
+    });
+}
+
+// Function to send a notification
+function sendNotification(title, message) {
+    Push.create(title, {
+        body: message,
+        timeout: 5000
+    });
+}
+
+document.addEventListener('permission-button-pressed', function (e) {
+    requestNotificationPermissions();
+});
+document.addEventListener('send-notification-button-pressed', function (e) {
+    sendNotification('Hey hoi hallo', 'hoe gaat ie dan');
+});
+document.addEventListener('read-notification-permissions-button-pressed', function (e) {
+    alert(Push.Permission.has() + ' ' + Push.Permission.get())
+});
