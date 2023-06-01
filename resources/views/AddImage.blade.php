b45513b63971db3f5d2d82621d3290a0256b3371
WaldoGames <48773478+WaldoGames@users.noreply.github.com>
2023-06-01 14:37:01 +0200

    verbeteringen

--- resources/views/AddImage.blade.php
+++ resources/views/AddImage.blade.php
@@ -5,13 +5,21 @@
 
     <!-- Container (Contact Section) -->
     <div id="contact" class="container">
-        <h1 class="text-center" style="margin-top: 100px">Image Upload</h1>
-
+        <h1 class="text-2xl text-center font-bold mt-10">Foto toevoegen</h1>
+        <p class="text-center italic uppercase text-hamburger w-3/4 mx-auto">product gekocht en heeft deze nog geen foto? u kunt deze zelf toevoegen!</p>
+        <p class="text-center text-xs text-hamburger w-3/4 mx-auto">deze foto zal zichtbaar zijn voor andere gebruikers. zorg dat de foto geen persoonlijke informatie bevat</p>
+      
         <form method="POST" action="{{ route('upload-image' , $productID) }}" enctype="multipart/form-data">
             @csrf
-            <input type="file" class="form-control" name="image" />
+            <div class="flex flex-col  content-center">
+            <input type="file" class=" mx-16 mt-10" class="form-control" name="image" />
+        <br>
 
-            <button type="submit" class="btn btn-sm">Upload</button>
+        <div class="flex flex-col gap-y-2 mt-3">
+            <button type="submit" class="btn m-2 bg-banner text-white  rounded-full p-1 text-center">Upload</button>
+        </div>
+           
+            </div>
         </form>
 
     </div>
--- resources/views/editProduct.blade.php
+++ resources/views/editProduct.blade.php
@@ -20,9 +20,13 @@
         </form>
         @if(!isset($product->image))
 
+        <div class=" m-4 border-2">
         <h2 class="font-bold text-center mt-10">lijkt er op dat er nog geen foto voor dit product bestaat. u kunt deze toevoegen als u wilt</h2>
-
-        <a href="{{ route('Product-image' , $product->id) }}" class="w-full mt-2 rounded-full text-center">Voegtoe</a>
+        <div class="flex m-3 flex-col gap-y-2 mt-3">
+            <a href="{{ route('Product-image' , $product->id) }}" class="w-full mt-2 mb-2 bg-banner text-white p-2 rounded-full text-center">Voegtoe</a>
+        </div>
+        </div>
+     
     
         @endif
     </div>
