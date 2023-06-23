fd6822df067898ef0f0b97c8bfecf89e2f1edf00
Jens Ramakers <jensramakers@gmail.com>
2023-06-23 15:24:02 +0200

    middleware

--- routes/web.php
+++ routes/web.php
@@ -25,13 +25,16 @@ Route::group(['middleware' => 'auth'], function () {
 Route::get('logout', [Login::class, 'LogoutUser'])->name('logout');
 Route::post('wait-for-connection/{accountID}', [Login::class, 'waitForResponse'])->name('login.wait-for-response');
 
-Route::get('shopping-list', [ShoppingListController::class, 'index'])->name('shopping-list.index');
-Route::post('shopping-list', [ShoppingListController::class, 'update'])->name('shopping-list.update');
 
-Route::get('inventory', [InventoryController::class, 'index'])->name('inventory.index');
-Route::get('inventory/{productID}/', [InventoryController::class, 'edit'])->name('inventory.edit');
-Route::post('inventory/{productID}', [InventoryController::class, 'update'])->name('inventory.update');
-Route::delete('inventory/{pivotId}', [InventoryController::class, 'destroy'])->name('inventory.destroy');
+Route::middleware('auth')->group(function () {
+    Route::get('shopping-list', [ShoppingListController::class, 'index'])->name('shopping-list.index');
+    Route::post('shopping-list', [ShoppingListController::class, 'update'])->name('shopping-list.update');
+
+    Route::get('inventory', [InventoryController::class, 'index'])->name('inventory.index');
+    Route::get('inventory/{productID}/', [InventoryController::class, 'edit'])->name('inventory.edit');
+    Route::post('inventory/{productID}', [InventoryController::class, 'update'])->name('inventory.update');
+    Route::delete('inventory/{pivotId}', [InventoryController::class, 'destroy'])->name('inventory.destroy');
+});
 
 Route::view('/connect', 'Login')->name('connect');
 Route::view('/connection-failed', 'LoginFailed')->name('connection-failed');
